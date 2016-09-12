fd5a57ba1143c87378009dffb724eb18bdbadf72
LM <loic.montcouquiol@2isa.org>
2016-09-12 14:26:00 +0200

    Création des classes Classement et Entrees

---
+++ Jeu2DesDLL/Classement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu2DesDLL
+{
+    public class Classement
+    {
+
+
+        #region "Propriétés et accesseurs"
+        private List<Entree> _Entrees;
+
+        public List<Entree> Entrees
+        {
+            get
+            {
+                return _Entrees;
+            }
+
+        }
+        #endregion
+        #region "Constructeurs"
+        public Classement()
+        {
+            _Entrees = new List<Entree>();
+        }
+        #endregion
+        #region "Methodes"
+        public void AjouterEntree(Joueur joueur)
+        {
+            Entree entree = new Entree(joueur);
+            Entrees.Add(entree);
+        }
+
+        public string TopN()
+        {
+            Entrees.Sort();
+            return Entrees.ToString();
+        }
+        #endregion
+        #region "Methodes héritées et substituées"
+        public override string ToString()
+        {
+            string retour = "";
+            foreach (Entree item in Entrees)
+            {
+                retour += "Nom : " + item.Nom + " Score : " + item.Score;
+            }
+            return retour;
+        }
+        #endregion
+        #region "Methodes à implementer pour les interfaces"
+        #endregion
+
+
+
+
+
+
+    }
+}
---
+++ Jeu2DesDLL/Entree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu2DesDLL
+{
+    public class Entree:IComparable
+    {
+
+        #region "Propriétés et accesseurs"
+        private string _Nom;
+        private int _Score;
+
+        public string Nom
+        {
+            get
+            {
+                return _Nom;
+            }
+
+            set
+            {
+                _Nom = value;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return _Score;
+            }
+
+            set
+            {
+                _Score = value;
+            }
+        }
+        #endregion
+        #region "Constructeurs"
+        public Entree(Joueur joueur)
+        {
+            this.Nom = joueur.Nom;
+            this.Score = joueur.Score;
+        }
+        #endregion
+        #region "Methodes"
+        #endregion
+        #region "Methodes héritées et substituées"
+        #endregion
+        #region "Methodes à implementer pour les interfaces"
+        public int CompareTo(object obj)
+        {
+            if (obj is Entree)
+            {
+                Entree other = (Entree)obj;
+                return this.Score.CompareTo(other.Score);
+            }
+            return -1;
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
