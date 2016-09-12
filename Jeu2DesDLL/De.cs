8cc20a7b5b40f8b2fda9051c635333dc716e9f26
LM <loic.montcouquiol@2isa.org>
2016-09-12 13:40:55 +0200

    Création de la solution et initialisation du Referentiel Git

--- Jeu2DesDLL/De.cs
+++ Jeu2DesDLL/De.cs
@@ -8,5 +8,31 @@ namespace Jeu2DesDLL
 {
     public class De
     {
+
+        #region "Propriétés et accesseurs"
+        private int _Valeur;
+
+        public int Valeur
+        {
+            get
+            {
+                return _Valeur;
+            }
+
+            set
+            {
+                _Valeur = value;
+            }
+        }
+        #endregion
+        #region "Constructeurs"
+        #endregion
+        #region "Methodes"
+        #endregion
+        #region "Methodes héritées et substituées"
+        #endregion
+        #region "Methodes à implementer pour les interfaces"
+        #endregion
+
     }
 }
